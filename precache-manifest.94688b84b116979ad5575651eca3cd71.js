d75e419e27e323899ea97384b4875f66a80d0c2b
Basia B <basiab222@gmail.com>
2020-05-25 11:58:42 -0700

    set homepage destionation fro build files

---
+++ precache-manifest.94688b84b116979ad5575651eca3cd71.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c40305d47df4969cbca18ac67d90ae2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "24a2768dc866eaa0780a",
+    "url": "./static/css/main.7190fe51.chunk.css"
+  },
+  {
+    "revision": "018c3185e8dc0628122e",
+    "url": "./static/js/2.0587572d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.0587572d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24a2768dc866eaa0780a",
+    "url": "./static/js/main.8d796b1a.chunk.js"
+  },
+  {
+    "revision": "45e37be234365194de07",
+    "url": "./static/js/runtime-main.f7411de3.js"
+  },
+  {
+    "revision": "c1b637fd385e0e42c37cbfc96538238e",
+    "url": "./static/media/amazon.c1b637fd.png"
+  },
+  {
+    "revision": "0d25cc45cdf313ef03b530088c810f14",
+    "url": "./static/media/applemusic.0d25cc45.svg"
+  },
+  {
+    "revision": "0eea0236e824027727777c637e1c283e",
+    "url": "./static/media/google.0eea0236.png"
+  },
+  {
+    "revision": "0e84d5926187e7a3b785febd3a55bf1d",
+    "url": "./static/media/instagram.0e84d592.png"
+  },
+  {
+    "revision": "0f2a6908a974057b9ddbe1c0cbc564a7",
+    "url": "./static/media/itunes.0f2a6908.svg"
+  },
+  {
+    "revision": "5e53d146a4143d5b64db00662eb9c5e8",
+    "url": "./static/media/laurent.5e53d146.png"
+  },
+  {
+    "revision": "49e37e37efa98b08c40cd2150c0058ab",
+    "url": "./static/media/soundcloud.49e37e37.png"
+  },
+  {
+    "revision": "11dc437ab8ef0c32289c092083d20068",
+    "url": "./static/media/spotify.11dc437a.png"
+  },
+  {
+    "revision": "690290e17a0a471070d2044e87fb2d67",
+    "url": "./static/media/tiktok.690290e1.svg"
+  },
+  {
+    "revision": "7ee556c2de1cafc11447059cfd4fa8a3",
+    "url": "./static/media/vital.7ee556c2.png"
+  },
+  {
+    "revision": "07b8560a6a6eb7155b80b0c599e6874e",
+    "url": "./static/media/youtube.07b8560a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.27bdc965277d646e6065b5b0516dcbac.js"
+  "./precache-manifest.94688b84b116979ad5575651eca3cd71.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
